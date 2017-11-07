0c7a130ff18e621493ecf107b355b10a2547f6e8
harvanjns <harvanjns@yahoo.com>
2017-11-07 13:37:36 +0700

    Update java-doc.

--- rumusan-core/src/main/java/org/rumusanframework/reflect/field/InLineResolver.java
+++ rumusan-core/src/main/java/org/rumusanframework/reflect/field/InLineResolver.java
@@ -17,14 +17,34 @@ public class InLineResolver extends DefaultFieldResolver {
      * Usage :
      * 
      * <pre>
-     * Class<?>[] fields = { ClientId.class, ResourceId.class, ClientSecret.class };
-     * String columns = ResolverUtils.concate(new InLineResolver(Dao.class, fields));
-     * StringBuilder sql = new StringBuilder();
-     * sql.append("select ").append(columns).append(" from ").append(OAuthClientDetails.class.getName());
-     * System.out.println(sql);
+     * public void method() {
+     * 	class AnyEntity {
+     * 		&#64Id;
+     *		private id;
+     *		&#64Desc;
+     *		private desc;
+     *  
+     *		&#64;Target({ FIELD })
+     *		&#64;Retention(RUNTIME)
+     *		public @interface Id {
+     *		}
+     *
+     *		&#64;Target({ FIELD })
+     *		&#64;Retention(RUNTIME)
+     *		public @interface Desc {
+     *		}
+     * 	}
+     * 
+     * 
+     * 	Class<?>[] fields = { Id.class, Desc.class};
+     * 	String columns = ResolverUtils.concate(new InLineResolver(Dao.class, fields));
+     * 	StringBuilder sql = new StringBuilder();
+     * 	sql.append("select ").append(columns).append(" from ").append(AnyEntity.class.getName());
+     * 	System.out.println(sql);
+     * }
      * 
      * will be print:
-     * select clientId,resourceIds,clientSecret from com.pasarsengget.module.domain.OAuthClientDetails
+     * select id,desc from org.rumusanframework.reflect.field.AnyEntity
      * </pre>
      * 
      * @param classUsage
