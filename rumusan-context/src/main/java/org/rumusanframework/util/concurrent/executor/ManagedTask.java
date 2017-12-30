467fef9e736f9bb2b152b9101bdc95df0e0a1487
Harvan Irsyadi <harvanjns@yahoo.com>
2017-12-30 16:12:12 +0700

    Init classes

---
+++ rumusan-context/src/main/java/org/rumusanframework/util/concurrent/executor/ManagedTask.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 29 Nov 2015 the original author or authors.
+ */
+
+package org.rumusanframework.util.concurrent.executor;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 
+ * @author Harvan Irsyadi
+ * @version 1.0
+ * 
+ */
+public class ManagedTask implements Runnable {
+    private static final Log LOGGER = LogFactory.getLog(ManagedTask.class);
+    private TaskEvent event;
+    private TaskEvent finalEvent;
+
+    public ManagedTask(TaskEvent event) {
+	this.event = event;
+    }
+
+    void setFinalEvent(TaskEvent finalEvent) {
+	this.finalEvent = finalEvent;
+    }
+
+    public final void run() {
+	try {
+	    event.execute();
+	} catch (Exception e) {
+	    if (logger().isErrorEnabled()) {
+		logger().error("Error on executing.", e);
+	    }
+	} finally {
+	    if (finalEvent != null) {
+		executeFinalEvent();
+	    }
+	}
+    }
+
+    private void executeFinalEvent() {
+	try {
+	    finalEvent.execute();
+	} catch (Exception e) {
+	    if (logger().isErrorEnabled()) {
+		logger().error("Error on executing final event.", e);
+	    }
+	}
+    }
+
+    Log logger() {
+	return LOGGER;
+    }
+}
---
+++ rumusan-core/src/test/java/org/rumusanframework/reflect/field/DefaultFieldResolverTest.java
@@ -0,0 +1,57 @@
+package org.rumusanframework.reflect.field;
+
+import java.util.Formatter;
+
+public class DefaultFieldResolverTest {
+    private static Formatter formatter = new Formatter();
+
+    public static void main(String[] args) {
+	Notification emailNotif = new EmailNotification();
+	printNotificationStatus(emailNotif);
+
+	emailNotif.markAsRead();
+	emailNotif.markAsRead();
+	printNotificationStatus(emailNotif);
+	System.out.println(formatter.format("%s%d", "oke", 10));
+    }
+
+    public static void printNotificationStatus(Notification notification) {
+	System.out.println(notification.status());
+    }
+}
+
+class Notification {
+    private String receiver;
+    private boolean isRead = false;
+
+    public Notification() {
+	// empty
+    }
+
+    public Notification(String receiver) {
+	this.receiver = receiver;
+    }
+
+    public boolean validReceiver() {
+	return receiver != null || receiver.trim().length() != 0;
+    }
+
+    public void markAsRead() {
+	if (isRead || !isRead) {
+	    isRead = true;
+	}
+    }
+
+    public String status() {
+	return isRead ? "read" : "unread";
+    }
+}
+
+class EmailNotification extends Notification {
+    private String concater = "-email";
+
+    @Override
+    public String status() {
+	return super.status().concat(concater);
+    }
+}
