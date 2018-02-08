b01935ceeeab3960e81d20b98677eb381ac12e56
Harvan Irsyadi <harvanjns@yahoo.com>
2018-02-08 16:01:33 +0700

    Add new utils.

---
+++ rumusan-core/src/main/java/org/rumusanframework/util/StringUtils.java
@@ -0,0 +1,17 @@
+package org.rumusanframework.util;
+
+/**
+ * 
+ * @author Harvan Irsyadi
+ * @version 1.0.0
+ *
+ */
+public class StringUtils {
+    private StringUtils() {
+	// hide
+    }
+
+    public static boolean isTrimEmpty(Object str) {
+	return (str == null || str.toString().trim().length() == 0);
+    }
+}
