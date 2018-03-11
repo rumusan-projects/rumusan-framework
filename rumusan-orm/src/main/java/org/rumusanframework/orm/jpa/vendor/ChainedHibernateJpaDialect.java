99f52e65b0dc6d7fd9dca78a7504d1b5c8e7fae5
Harvan Irsyadi <harvanjns@yahoo.com>
2018-03-12 01:12:53 +0700

    rumusan-orm : First commit.

---
+++ rumusan-orm/src/main/java/org/rumusanframework/orm/jpa/vendor/ChainedHibernateJpaDialect.java
@@ -0,0 +1,39 @@
+package org.rumusanframework.orm.jpa.vendor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.support.PersistenceExceptionTranslator;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+
+/**
+ * 
+ * @author Harvan Irsyadi
+ * @version 1.0.0
+ *
+ */
+public class ChainedHibernateJpaDialect extends HibernateJpaDialect {
+    private static final long serialVersionUID = 4425692243075506813L;
+    private transient Set<PersistenceExceptionTranslator> translators;
+
+    public void addTranslator(PersistenceExceptionTranslator translator) {
+	if (translators == null) {
+	    translators = new HashSet<>();
+	}
+
+	translators.add(translator);
+    }
+
+    @Override
+    public DataAccessException translateExceptionIfPossible(RuntimeException ex) {
+	for (PersistenceExceptionTranslator pet : translators) {
+	    DataAccessException dae = pet.translateExceptionIfPossible(ex);
+	    if (dae != null) {
+		return dae;
+	    }
+	}
+
+	return super.translateExceptionIfPossible(ex);
+    }
+}
